7875f4d2f382e959d9fd190037facaa4ddb64464
SuperCoolMan <101731621+iSuperCoolMan@users.noreply.github.com>
2025-09-19 12:55:30 +0400

    Update ai.py

--- ai.py
+++ ai.py
@@ -21,13 +21,13 @@ def create_deformation(think: str):  # создаем ввод текста
 
     promt2 = f"Опиши, почему ты сделал такой выбор?"
 
-    chat1 = openai.chat.completions.create(model="gpt-3.5-turbo", messages=[
+    chat1 = openai.chat.completions.create(model="gpt-oss-20b", messages=[
         {"role": "user", "content": promt1}
     ])
 
     deformation = chat1.choices[0].message.content
 
-    chat2 = openai.chat.completions.create(model="gpt-3.5-turbo", messages=[
+    chat2 = openai.chat.completions.create(model="gpt-oss-20b", messages=[
         {"role": "user", "content": promt1},
         {"role": "assistant", "content": deformation},
         {"role": "user", "content": promt2}
